5c3afe204ac1ae720d1ce66cbee6bb9607c6799f
Rafael Sanchez <rfsanchezb@unal.edu.co>
2015-11-01 16:49:55 -0500

    rama cu2_cu3 = rama cu1 + phps y tpls registra_animal y registrar_dueño

--- perfil_veterinario.php
+++ perfil_veterinario.php
@@ -5,8 +5,10 @@ require('configs/include.php');
 class c_perfil_veterinario extends super_controller {
     
     public function display(){
-
-        $this->engine->display('cabecera_veterinario.tpl');
+        $this->engine->assign('title', "Perfil Veterinario");
+        $this->engine->assign('nombre',$this->session['usuario']['nombre']);
+        $this->engine->assign('tipo',$this->session['usuario']['tipo']);
+        $this->engine->display('cabecera.tpl');
         $this->engine->display('perfil_veterinario.tpl');
         $this->engine->display('piedepagina.tpl');
     
