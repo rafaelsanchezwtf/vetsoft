192f02deee9ed31d4bf8d9090ab12a1e45fabcb4
Rafael Sanchez <rfsanchezb@unal.edu.co>
2015-11-01 16:52:10 -0500

    index modificado, ahora si cu2_cu3 está igual a cu1 mas los casos de uso registrar dueño y animal

--- index.php
+++ index.php
@@ -4,14 +4,26 @@ require('configs/include.php');
 
 class c_prueba extends super_controller {
 
-
-    
-    
     public function display(){
-        
-        $this->engine->assign('title',$this->gvar['n_index']);
-        $this->engine->display('cabecera.tpl');
-        $this->engine->display('piedepagina.tpl');
+
+        $this->engine->assign('tipo',$this->session['usuario']['tipo']);
+        $this->engine->assign('nombre',$this->session['usuario']['nombre']);
+        if ($this->session['usuario']['tipo']=="administrador") {
+            $this->engine->assign('title', "Perfil Administrador");
+            $this->engine->display('cabecera.tpl');
+            $this->engine->display('perfil_administrador.tpl');
+            $this->engine->display('piedepagina.tpl');   
+        }elseif ($this->session['usuario']['tipo']=="veterinario") {
+            $this->engine->assign('title', "Perfil Veterinario");
+            $this->engine->display('cabecera.tpl');
+            $this->engine->display('perfil_veterinario.tpl');
+            $this->engine->display('piedepagina.tpl');    
+        }else{
+            $this->engine->assign('title', "Home");
+            $this->engine->assign('title',$this->gvar['n_index']);
+            $this->engine->display('cabecera.tpl');
+            $this->engine->display('piedepagina.tpl');
+        }
     
     }
     
