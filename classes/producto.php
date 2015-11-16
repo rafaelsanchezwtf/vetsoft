b00fd37d408bbd04722234c22160ab1f183a2167
Rafael Sanchez <rfsanchezb@unal.edu.co>
2015-11-16 18:35:54 -0500

    funciones de validacion de completitud y correctitud para producto creadas

--- classes/producto.php
+++ classes/producto.php
@@ -21,6 +21,35 @@
 			return array("id");
 		}
 
+		public static function validar_completitud($producto){
+			$flag = FALSE;
+			if (is_empty($producto->get('nombre'))){
+            	$flag = TRUE;
+        	}
+        	if (is_empty($producto->get('cantidad'))){
+            	$flag = TRUE;
+	        }
+	        if (is_empty($producto->get('marca'))){
+	            $flag = TRUE;
+	        }
+	        if ($producto->get('tipo') == "seleccion"){
+	            $flag = TRUE;
+	        }
+        	RETURN $flag;
+		}
+
+		public static function validar_correctitud($producto){
+    		$flag = FALSE;
+    		if ((!is_numeric($producto->get('cantidad'))) OR ($producto->get('cantidad') <= 0)){
+	            $flag = TRUE;     
+	        }
+
+	        if (($producto->get('tipo') != "medicamento") AND ($producto->get('tipo') != "implemento")){
+	            $flag = TRUE;   
+        	}
+	        RETURN $flag;
+    	}
+
 		public function relational_keys($class, $rel_name){
 			switch ($class) {
 				
